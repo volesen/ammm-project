3480febad71f5f2fb1caa3e3bef3787e62b6b2b2
volesen <vincolesen@gmail.com>
2024-04-26 12:08:45 +0200

    Implemented naive greedy search

---
+++ src/ammm_project/greedy.py
@@ -0,0 +1,35 @@
+from ammm_project.problem import Square, Suitcase
+
+
+def q(square, suitcase):
+    """
+    Greedy function
+    """
+    return square.price / square.side**2
+
+
+def greedy_search(suitcase: Suitcase, squares: list[Square]):
+    """
+    Greedy search
+    """
+
+    # Sort the squares by decreasing price
+    squares.sort(key=lambda square: q(square, suitcase), reverse=True)
+
+    # Add the squares to the suitcase
+    for square in squares:
+        if not suitcase.is_below_weight_limit(square):
+            # We can remove this square from the list of squares
+            # because it will never fit in the suitcase
+            continue
+
+        for x, y in suitcase.free_cells:
+            if suitcase.can_fit_square(square, x, y):
+                suitcase.add_square(square, x, y)
+                break
+        else:
+            # We can remove this square from the list of squares
+            # because it will never fit in the suitcase
+            continue
+
+    return suitcase
---
+++ src/ammm_project/problem.py
@@ -0,0 +1,102 @@
+class Square:
+    id: int
+    price: int
+    weight: int
+    side: int
+
+    def __init__(self, id: int, price: int, weight: int, side: int):
+        self.id = id
+        self.price = price
+        self.weight = weight
+        self.side = side
+
+
+class Suitcase:
+    """
+    A suitcase is a grid of WxH cells, where each cell can be either empty or occupied by a square.
+    """
+
+    # Immutable properties
+    width: int
+    height: int
+    max_weight: int
+
+    # Mutable properties
+    free_cells: set[tuple[int, int]]
+
+    weight: int
+    value: int
+    contents: set[int]
+
+    def __init__(self, width: int, height: int, max_weight: int):
+        self.width = width
+        self.height = height
+        self.max_weight = max_weight
+
+        self.free_cells = {(x, y) for x in range(width) for y in range(height)}
+
+        self.weight = 0
+        self.value = 0
+        self.contents = set()
+
+    def can_fit_square(self, square: Square, x: int, y: int) -> bool:
+        """
+        Check if the square can fit in the suitcase at position (x, y).
+        """
+        return all(
+            0 <= x + dx < self.width and 0 <= y + dy < self.height
+            for dx in range(square.side)
+            for dy in range(square.side)
+        )
+
+    def is_below_weight_limit(self, square: Square) -> bool:
+        """
+        Check if adding the square keeps the total weight below the maximum allowed weight.
+        """
+        return self.weight + square.weight <= self.max_weight
+
+    def add_square(self, square: Square, x: int, y: int):
+        self.contents.add(square.id)
+
+        self.weight += square.weight
+        self.value += square.price
+
+        self.free_cells.update(
+            {(x + dx, y + dy) for dx in range(square.side) for dy in range(square.side)}
+        )
+
+    def remove_square(self, square: Square, x: int, y: int):
+        self.contents.remove(square.id)
+
+        self.weight -= square.weight
+        self.value -= square.price
+
+        self.free_cells.difference_update(
+            {(x + dx, y + dy) for dx in range(square.side) for dy in range(square.side)}
+        )
+
+    def __contains__(self, square: Square) -> bool:
+        return square.id in self.contents
+
+    def __str__(self) -> str:
+        return f"Suitcase(width={self.width}, height={self.height}, max_weight={self.max_weight}, weight={self.weight}, value={self.value}, contents={self.contents})"
+
+
+def from_dat(dat: dict):
+    suitcase = Suitcase(
+        width=dat["x"],
+        height=dat["y"],
+        max_weight=dat["c"],
+    )
+
+    squares = [
+        Square(
+            id=i,
+            price=dat["p"][i],
+            weight=dat["w"][i],
+            side=dat["s"][i],
+        )
+        for i in range(dat["n"])
+    ]
+
+    return suitcase, squares
