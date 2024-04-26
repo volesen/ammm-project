e9eef066781b598a56e211e9f69b80a9849042fd
volesen <vincolesen@gmail.com>
2024-04-26 12:11:34 +0200

    Add script to run greedy search

--- src/ammm_project/__init__.py
+++ src/ammm_project/__init__.py
@@ -1,3 +1,22 @@
+import argparse
+import ammm_project.parsers.dat
+import ammm_project.greedy
+import ammm_project.problem
+
+
 def main() -> int:
-    print("Hello from ammm-project!")
+    parser = argparse.ArgumentParser(description="AMMM project")
+    # .dat file
+    parser.add_argument("dat", type=str, help="The .dat file to solve")
+    args = parser.parse_args()
+
+    with open(args.dat, "r") as f:
+        dat = ammm_project.parsers.dat.parse(f)
+
+    suitcase, squares = ammm_project.problem.from_dat(dat)
+
+    suitcase = ammm_project.greedy.greedy_search(suitcase, squares)
+
+    print(suitcase)
+
     return 0
