ceec6dc51cb619e55d4e3e0038a78c58c095abf9
volesen <vincolesen@gmail.com>
2024-04-26 12:11:24 +0200

    Add comment about ordered set

--- src/ammm_project/greedy.py
+++ src/ammm_project/greedy.py
@@ -23,6 +23,7 @@ def greedy_search(suitcase: Suitcase, squares: list[Square]):
             # because it will never fit in the suitcase
             continue
 
+        # We exploit the fact that the free cells are sorted (but officially they are not)
         for x, y in suitcase.free_cells:
             if suitcase.can_fit_square(square, x, y):
                 suitcase.add_square(square, x, y)
