2b4adf4e6bb6a9a59cf1ab902fe05ca300e8ebde
volesen <vincolesen@gmail.com>
2024-05-02 14:11:44 +0200

    Update GRASP to run on smaller instances

--- tune-grasp.sh
+++ tune-grasp.sh
@@ -1,6 +1,7 @@
 mkdir -p grasp_tuning
+
 for alpha in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-    for instance in grasp_tuning/mix-100-*.dat; do
+    for instance in grasp_tuning/*.dat; do
         echo "Tuning $instance $alpha"
         rye run ammm-project --algorithm grasp --alpha $alpha $instance > grasp_tuning/$(basename $instance .dat)-$alpha.out
     done
