0571d97ed9830384d55338ae98a079d02fd1ed7c
volesen <vincolesen@gmail.com>
2024-05-02 12:53:11 +0200

    Update generate instances

--- generate-instances.sh
+++ generate-instances.sh
@@ -6,8 +6,8 @@ for algorithm in "knapsack" "bin_packing" "mix"; do
     for size in 50 100 200; do
         for i in $(seq 1 $NUM_INSTANCES_PER_SIZE); do
             echo "Generating instance $algorithm $size $i"
-            
-            rye run generate-instance mix $size > instances/$algorithm-$size-$i.dat
+
+            rye run generate-instance --algorithm $algorithm $size > instances/$algorithm-$size-$i.dat
         done
     done
 done
