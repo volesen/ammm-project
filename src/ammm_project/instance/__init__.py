9c8cd90240437fd6e065929826cb1a4ded958690
volesen <vincolesen@gmail.com>
2024-05-02 12:55:20 +0200

    Upcate CLI

--- src/ammm_project/instance/__init__.py
+++ src/ammm_project/instance/__init__.py
@@ -6,14 +6,11 @@ from .serialize import serialize
 from .knapsack import generate as generate_knapsack_instance
 from .bin_packing import generate as generate_bin_packing_instance
 from .mix import generate as generate_mix_instance
-from .suitcase import generate as generate_suitcase_instance
 
 class InstanceGenerator(enum.Enum):
     KNAPSACK = "knapsack"
     BIN_PACKING = "bin_packing"
     MIX = "mix"
-    SUITCASE = "suitcase"
-
 
 def main():
     parser = argparse.ArgumentParser(
@@ -44,9 +41,7 @@ def main():
         instance = generate_bin_packing_instance(args.size)
     elif args.generator == InstanceGenerator.MIX:
         instance = generate_mix_instance(args.size)
-    elif args.generator == InstanceGenerator.SUITCASE:
-        instance = generate_suitcase_instance(args.size)
-
+ 
     print(serialize(instance))
 
     sys.exit(0)
