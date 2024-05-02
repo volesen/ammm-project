53ed97fc69653754fb756474f4cb1e0a3f4e293a
volesen <vincolesen@gmail.com>
2024-05-02 10:51:01 +0200

    Add knapsack instance generation

---
+++ src/ammm_project/instance/knapsack.py
@@ -0,0 +1,21 @@
+import random
+
+
+def generate(num_items: int):
+    prices = [random.randint(1, 100) for _ in range(num_items)]
+    weights = [random.randint(1, 100) for _ in range(num_items)]
+    sides = [1 for _ in range(num_items)]
+
+    capacity_fraction = random.uniform(0, 1)
+
+    max_weight = int(sum(weights) * capacity_fraction)
+
+    return {
+        "x": 1,
+        "y": num_items,
+        "n": num_items,
+        "c": max_weight,
+        "w": weights,
+        "p": prices,
+        "s": sides,
+    }
