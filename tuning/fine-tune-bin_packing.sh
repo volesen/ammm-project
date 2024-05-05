c15a7747675bcb6073d532b3dc554b92d3e2bcb0
volesen <vincolesen@gmail.com>
2024-05-05 16:54:22 +0200

    Limit tuning if no improvement

--- tuning/fine-tune-bin_packing.sh
+++ tuning/fine-tune-bin_packing.sh
@@ -4,6 +4,6 @@ for alpha in 0.91 0.92 0.93 0.94 0.95 0.96 0.97 0.98 0.99; do
     for i in {1..5}; do
         instance="instances/bin_packing-50-1.dat"
         echo "Finetune $instance $alpha"
-        rye run ammm-project --algorithm grasp --alpha $alpha $instance > grasp_tuning/$(basename $instance .dat)-$alpha-$i.out
+        rye run ammm-project --algorithm grasp --alpha $alpha $instance --max-time 300 --max-time-since-improvement 60 > grasp_tuning/$(basename $instance .dat)-$alpha-$i.out
     done
 done
--- tuning/tune-bin_packing.sh
+++ tuning/tune-bin_packing.sh
@@ -4,6 +4,6 @@ for alpha in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
     for i in {1..5}; do
         instance="instances/bin_packing-50-1.dat"
         echo "Tuning $instance $alpha"
-        rye run ammm-project --algorithm grasp --alpha $alpha $instance > grasp_tuning/$(basename $instance .dat)-$alpha-$i.out
+        rye run ammm-project --algorithm grasp --alpha $alpha $instance --max-time 300 --max-time-since-improvement 60 > grasp_tuning/$(basename $instance .dat)-$alpha-$i.out
     done
 done
