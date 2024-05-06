9c36405037d708bb7f57f6cedf60dd2b0c014b3b
volesen <vincolesen@gmail.com>
2024-05-06 11:14:58 +0200

    Update heuristics benchmark

--- benchmarks-heuristic.sh
+++ benchmarks-heuristic.sh
@@ -1,14 +1,14 @@
 # Greedy
-mkdir -p benchmarks
+mkdir -p benchmarks_heuristic
 
 echo "Greedy"
 for instance in instances/*-2.dat; do
     echo "Benchmarking $instance"
-    rye run ammm-project --algorithm greedy $instance > benchmarks/greedy-$(basename $instance .dat).out
+    rye run ammm-project --algorithm greedy $instance > benchmarks_heuristic/greedy-$(basename $instance .dat).out
 done
 
 echo "Greedy with local search"
 for instance in instances/*-2.dat; do
     echo "Benchmarking $instance"
-    rye run ammm-project --algorithm greedy_with_local_search $instance > benchmarks/greedy_with_local_search-$(basename $instance .dat).out
+    rye run ammm-project --algorithm greedy_with_local_search $instance > benchmarks_heuristic/greedy_with_local_search-$(basename $instance .dat).out
 done
