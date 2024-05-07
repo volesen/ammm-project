cf6de14b135b29b1d4cc094b6fbccb9ba44463dd
volesen <vincolesen@gmail.com>
2024-05-07 17:01:40 +0200

    Update reference benchmark

--- reference-benchmarks-cplex.sh
+++ reference-benchmarks-cplex.sh
@@ -23,3 +23,9 @@ for instance in reference/*.dat; do
     echo "Benchmarking $instance"
     /Applications/CPLEX_Studio2211/opl/bin/arm64_osx/oplrun opl/bigM2.mod $instance > reference-benchmarks/bigM2-$(basename $instance .dat).out
 done
+
+echo "Cplex bigM3"
+for instance in reference/*.dat; do
+    echo "Benchmarking $instance"
+    /Applications/CPLEX_Studio2211/opl/bin/arm64_osx/oplrun opl/bigM3.mod $instance > reference-benchmarks/bigM3-$(basename $instance .dat).out
+done
