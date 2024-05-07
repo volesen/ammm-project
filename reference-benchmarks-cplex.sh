202dcd370a06a7ad61d4c14d7a3a3f8c9a7e6f3e
volesen <vincolesen@gmail.com>
2024-05-07 15:58:17 +0200

    Add new benchamrk script

---
+++ reference-benchmarks-cplex.sh
@@ -0,0 +1,25 @@
+mkdir -p reference-benchmarks
+
+echo "Cplex binary"
+for instance in reference/*.dat; do
+    echo "Benchmarking $instance"
+    /Applications/CPLEX_Studio2211/opl/bin/arm64_osx/oplrun opl/binary.mod $instance > reference-benchmarks/binary-$(basename $instance .dat).out
+done
+
+echo "Cplex bigM"
+for instance in reference/*.dat; do
+    echo "Benchmarking $instance"
+    /Applications/CPLEX_Studio2211/opl/bin/arm64_osx/oplrun opl/bigM.mod $instance > reference-benchmarks/bigM-$(basename $instance .dat).out
+done
+
+echo "Cplex bigM1"
+for instance in reference/*.dat; do
+    echo "Benchmarking $instance"
+    /Applications/CPLEX_Studio2211/opl/bin/arm64_osx/oplrun opl/bigM1.mod $instance > reference-benchmarks/bigM1-$(basename $instance .dat).out
+done
+
+echo "Cplex bigM2"
+for instance in reference/*.dat; do
+    echo "Benchmarking $instance"
+    /Applications/CPLEX_Studio2211/opl/bin/arm64_osx/oplrun opl/bigM2.mod $instance > reference-benchmarks/bigM2-$(basename $instance .dat).out
+done
